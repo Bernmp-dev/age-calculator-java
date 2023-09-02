cf9f0703ec5a0a9e775cb17249ed35347531fdb3
eon <bernardomp.dev@gmail.com>
2023-09-01 21:08:25 -0300

    Feat: AgeCalculatorService.

--- src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
+++ src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
@@ -1,13 +1,23 @@
 package com.betrybe.calcuradoradeidade.service;
 
+import com.betrybe.calcuradoradeidade.dto.DateDto;
+import java.time.LocalDate;
+import java.time.Period;
+import org.springframework.stereotype.Service;
+
 /**
  * Aqui nessa classe devem ser implementados os métodos para atender aos requisitos do exercício,
  * sinta-se à vontade para criar métodos privados para te auxiliar nas validações.
  */
+@Service
 public class AgeCalculatorService {
+
+  /** calculateAge. */
   public int calculateAge(String date) {
-    // TODO method implementation
-    return -1;
+    LocalDate localDate = LocalDate.parse(date);
+    LocalDate now = LocalDate.now();
+
+    return Period.between(localDate, now).getYears();
   }
 
   public int calculateAgeWithDefault(String date, int defaultAge) {
