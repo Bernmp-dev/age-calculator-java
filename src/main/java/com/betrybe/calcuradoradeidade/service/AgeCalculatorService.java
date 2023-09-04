825e55bdd773a7fcdd8e3986b85950accee20711
eon <bernardomp.dev@gmail.com>
2023-09-04 16:47:22 -0300

    [AgeCalculatorService]Feat: validateSyntax.

--- src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
+++ src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
@@ -1,6 +1,7 @@
 package com.betrybe.calcuradoradeidade.service;
 
 import com.betrybe.calcuradoradeidade.exception.FutureDateException;
+import com.betrybe.calcuradoradeidade.exception.InvalidSyntaxDateEx;
 import java.time.LocalDate;
 import java.time.Period;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,9 @@ import org.springframework.stereotype.Service;
 public class AgeCalculatorService {
 
   /** calculateAge. */
-  public int calculateAge(String date) throws FutureDateException {
+  public int calculateAge(String date) {
+    validateSyntax(date);
+
     LocalDate localDate = LocalDate.parse(date);
     LocalDate now = LocalDate.now();
     int years = Period.between(localDate, now).getYears();
@@ -28,5 +31,18 @@ public class AgeCalculatorService {
   public int calculateAgeWithDefault(String date, int defaultAge) {
     // TODO method implementation
     return -1;
+  }
+
+  private void validateSyntax(String date) throws InvalidSyntaxDateEx {
+    String[] dateBlocks = date.split("-");
+    if (dateBlocks.length != 3) {
+      throw new InvalidSyntaxDateEx();
+    }
+    String year = dateBlocks[0];
+    String month = dateBlocks[1];
+    String day = dateBlocks[2];
+    if (year.length() != 4 || month.length() != 2 || day.length() != 2) {
+      throw new InvalidSyntaxDateEx();
+    }
   }
 }
