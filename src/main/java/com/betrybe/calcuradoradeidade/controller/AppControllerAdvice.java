f8a54d8f6151e2f6e4ba5c7c45c01c6974206dbf
eon <bernardomp.dev@gmail.com>
2023-09-04 16:49:23 -0300

    [AppControllerDevice]Feat: handleInvalidSyntaxEx.

--- src/main/java/com/betrybe/calcuradoradeidade/controller/AppControllerAdvice.java
+++ src/main/java/com/betrybe/calcuradoradeidade/controller/AppControllerAdvice.java
@@ -2,17 +2,25 @@ package com.betrybe.calcuradoradeidade.controller;
 
 import com.betrybe.calcuradoradeidade.dto.ErrorMessageDto;
 import com.betrybe.calcuradoradeidade.exception.FutureDateException;
+import com.betrybe.calcuradoradeidade.exception.InvalidSyntaxDateEx;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/** AppControllerAdvice. */
 @RestControllerAdvice
 public class AppControllerAdvice {
 
   @ExceptionHandler(FutureDateException.class)
   @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
   public ErrorMessageDto handleFutureDateEx(RuntimeException exception) {
+    return new ErrorMessageDto(exception.getMessage());
+  }
+
+  @ExceptionHandler(InvalidSyntaxDateEx.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorMessageDto handleInvalidSyntaxDateEx(RuntimeException exception) {
     return new ErrorMessageDto(exception.getMessage());
   }
 }
