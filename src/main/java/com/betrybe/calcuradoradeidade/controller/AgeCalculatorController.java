05ac48632374046a9b818e12869283b96a15e400
eon <bernardomp.dev@gmail.com>
2023-09-04 21:59:25 -0300

    [AgeCalculatorController]Refactor: calculateAge.

--- src/main/java/com/betrybe/calcuradoradeidade/controller/AgeCalculatorController.java
+++ src/main/java/com/betrybe/calcuradoradeidade/controller/AgeCalculatorController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +26,14 @@ public class AgeCalculatorController implements AgeCalculatorControllerInterface
    */
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public DateDto calculateAge(@RequestParam String date, String orDefaultAge) {
-    return new DateDto(service.calculateAge(date));
+  public DateDto calculateAge(String date, String orDefaultAge) {
+    if (orDefaultAge == null) {
+      return new DateDto(service.calculateAge(date));
+    }
+
+    return new DateDto(
+        service.calculateAgeWithDefault(
+        date, Integer.parseInt(orDefaultAge)
+        ));
   }
 }
